f3556d3fbf9b76d9e55008e5d5a353d04ff6522b
Anas Siddiqui <anassiddiqui0910@gmail.com>
2024-06-03 23:06:51 +0500

    Update comment.controller.js

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -33,10 +33,48 @@ const addComment = asyncHandler(async (req, res) => {
 
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
+
+  const { commentId } = req.params;
+  const { updatedComment } = req.body;
+
+  console.log("comment id", commentId);
+  console.log("updated comment", updatedComment);
+
+  if (!commentId) throw new ApiError(200, "comment id not provided");
+
+  if (!updatedComment) throw new ApiError(200, "comment is missing");
+
+  const comment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      content: updatedComment,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!comment) throw new ApiError("issue while updating the comment");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "comment updated successfully"));
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
+
+  const { commentId } = req.params;
+
+  if (!commentId) throw new ApiError(400, "comment id is required");
+
+  const comment = await Comment.findByIdAndDelete(commentId);
+
+  if (!comment) throw new ApiError(400, "issue while deleting the comment");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "comment deleted successfully"));
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
