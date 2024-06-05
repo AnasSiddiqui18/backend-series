5b6af1ad3ab290015818a053b0514159bc05d879
Anas Siddiqui <anassiddiqui0910@gmail.com>
2024-06-05 16:07:13 +0500

    healtCheck + dashboard controller added

--- src/app.js
+++ src/app.js
@@ -23,6 +23,10 @@ import commentRouter from "./routes/comment.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
+
+// import communityRouter from "./routes/community.routes.js";
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
@@ -31,8 +35,8 @@ app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/playlists", playlistRouter);
-
-// import communityRouter from "./routes/community.routes.js";
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 // app.use("/api/v1/communities", communityRouter);
 
--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,20 +1,96 @@
-// import mongoose from "mongoose"
-// import {Video} from "../models/video.model.js"
-// import {Subscription} from "../models/subscription.model.js"
-// import {Like} from "../models/like.model.js"
-// import {ApiError} from "../utils/ApiError.js"
-// import {ApiResponse} from "../utils/ApiResponse.js"
-// import {asyncHandler} from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-// const getChannelStats = asyncHandler(async (req, res) => {
-//     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-// })
+const getChannelStats = asyncHandler(async (req, res) => {
+  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 
-// const getChannelVideos = asyncHandler(async (req, res) => {
-//     // TODO: Get all the videos uploaded by the channel
-// })
+  const currentUser = req.user._id;
 
-// export {
-//     getChannelStats, 
-//     getChannelVideos
-//     }
+  console.log("current user", currentUser);
+
+  const video = await Video.find({ owner: currentUser });
+
+  console.log("video", video);
+
+  const getChannelVideos = await Video.aggregate([
+    {
+      $match: {
+        owner: currentUser,
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "owner",
+        foreignField: "channel",
+        as: "subscribers",
+
+        pipeline: [
+          {
+            $project: {
+              subscriber: 1,
+            },
+          },
+        ],
+      },
+    },
+
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+      },
+    },
+
+    {
+      $project: {
+        likes: 1,
+        subscribersCount: 1,
+      },
+    },
+  ]);
+
+  console.log("get channel videos", getChannelVideos);
+
+  const totalLikes = getChannelVideos.reduce((acc, currentValue) => {
+    return currentValue.likes.length + acc;
+  }, 0);
+
+  res.status(200).json({
+    totalSubscribers: getChannelVideos[0].subscribersCount,
+    totalVideos: getChannelVideos?.length,
+    totalLikes: totalLikes,
+    message: "Channel stats fetched successfully",
+  });
+});
+
+const getChannelVideos = asyncHandler(async (req, res) => {
+  // TODO: Get all the videos uploaded by the channel
+
+  const currentUser = req.user._id;
+  const channelVideos = await Video.find({ owner: currentUser });
+
+  if (channelVideos?.length < 1) {
+    throw new ApiError(400, "videos not found");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channelVideos, "channel videos fetched successfully")
+    );
+});
+
+export { getChannelStats, getChannelVideos };
